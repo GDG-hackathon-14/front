a863d2b69ab140efd237fade8a79cec57de08504
devohda <fffvvv6@gmail.com>
2022-06-26 03:52:34 +0900

    localstorage 저장

--- src/pages/company.js
+++ src/pages/company.js
@@ -128,6 +128,7 @@ function Company() {
           onClick={() => {
             if (company) {
               navigate("/write-form");
+              window.localStorage.setItem("company", name);
             }
           }}
         >
